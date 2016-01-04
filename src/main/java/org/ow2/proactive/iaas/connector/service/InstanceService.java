3b85bfc0c298b3fd88cfbfa090312ede4366ff7c
Marco Castigliego <marcocast@gmail.com>
2016-01-04 12:14:51 +0100

    removed wrong dependency

--- src/main/java/org/ow2/proactive/iaas/connector/service/InstanceService.java
+++ src/main/java/org/ow2/proactive/iaas/connector/service/InstanceService.java
@@ -4,71 +4,68 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.jclouds.compute.ComputeService;
-import org.jclouds.compute.RunNodesException;
-import org.jclouds.compute.domain.ComputeMetadata;
-import org.jclouds.compute.domain.ComputeMetadataIncludingStatus;
 import org.jclouds.compute.domain.NodeMetadata;
-import org.jclouds.compute.domain.NodeMetadata.Status;
 import org.jclouds.compute.domain.Template;
-import org.jclouds.compute.domain.internal.ComputeMetadataImpl;
 import org.jclouds.compute.domain.internal.NodeMetadataImpl;
-import org.json.JSONObject;
 import org.ow2.proactive.iaas.connector.cache.ComputeServiceCache;
 import org.ow2.proactive.iaas.connector.model.Instance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.testng.internal.annotations.Sets;
+
+import com.google.common.collect.Sets;
+
 
 @Service
 public class InstanceService {
 
-	@Autowired
-	private InfrastructureService infrastructureService;
+    @Autowired
+    private InfrastructureService infrastructureService;
 
-	@Autowired
-	private ComputeServiceCache computeServiceCache;
+    @Autowired
+    private ComputeServiceCache computeServiceCache;
 
-	public Instance createInstance(Instance instance) {
-		ComputeService computeService = getComputeServiceFromInfastructureName(instance.getInfrastructureName());
+    public Instance createInstance(Instance instance) {
+        ComputeService computeService = getComputeServiceFromInfastructureName(
+                instance.getInfrastructureName());
 
-		Template template = computeService.templateBuilder().minRam(Integer.parseInt(instance.getRam()))
-				.imageId(instance.getImage()).build();
+        Template template = computeService.templateBuilder().minRam(Integer.parseInt(instance.getRam()))
+                .imageId(instance.getImage()).build();
 
-		Set<? extends NodeMetadata> createdNodeMetaData = Sets.newHashSet();
-		
-		try {
-			 createdNodeMetaData = computeService.createNodesInGroup(instance.getName(), Integer.parseInt(instance.getNumber()), template);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		
-		return createdNodeMetaData.stream().findFirst().map(nodeMetaData -> instance.withId(nodeMetaData.getId())).get();
+        Set<? extends NodeMetadata> createdNodeMetaData = Sets.newHashSet();
 
-	}
+        try {
+            createdNodeMetaData = computeService.createNodesInGroup(instance.getName(),
+                    Integer.parseInt(instance.getNumber()), template);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-	public void deleteInstance(String infrastructureName, String instanceId) {
-		getComputeServiceFromInfastructureName(infrastructureName).destroyNode(instanceId);
-	}
+        return createdNodeMetaData.stream().findFirst()
+                .map(nodeMetaData -> instance.withId(nodeMetaData.getId())).get();
 
-	public Set<Instance> getAllInstances(String infrastructureName) {
-	
-		return getComputeServiceFromInfastructureName(infrastructureName).listNodes().stream()
-				.map(computeMetadata -> (NodeMetadataImpl)computeMetadata)
-				.map(nodeMetadataImpl -> Instance.builder()
-						.id(nodeMetadataImpl.getId())
-						.name(nodeMetadataImpl.getName())
-						.image(nodeMetadataImpl.getImageId())
-						.number("1")
-						.ram(String.valueOf(nodeMetadataImpl.getHardware().getRam()))
-						.cpu(String.valueOf(nodeMetadataImpl.getHardware().getProcessors().size()))
-						.status(nodeMetadataImpl.getStatus().name())
-						.infrastructureName(infrastructureName)
-						.build())
-				.collect(Collectors.toSet());
-	}
+    }
 
-	private ComputeService getComputeServiceFromInfastructureName(String infrastructureName) {
-		return computeServiceCache.getComputeService(infrastructureService.getInfrastructurebyName(infrastructureName));
-	}
+    public void deleteInstance(String infrastructureName, String instanceId) {
+        getComputeServiceFromInfastructureName(infrastructureName).destroyNode(instanceId);
+    }
+
+    public Set<Instance> getAllInstances(String infrastructureName) {
+
+        return getComputeServiceFromInfastructureName(infrastructureName).listNodes()
+                .stream().map(
+                        computeMetadata -> (NodeMetadataImpl) computeMetadata)
+                .map(nodeMetadataImpl -> Instance.builder().id(nodeMetadataImpl.getId())
+                        .name(nodeMetadataImpl.getName()).image(nodeMetadataImpl.getImageId()).number("1")
+                        .ram(String.valueOf(nodeMetadataImpl.getHardware().getRam()))
+                        .cpu(String.valueOf(nodeMetadataImpl.getHardware().getProcessors().size()))
+                        .status(nodeMetadataImpl.getStatus().name()).infrastructureName(infrastructureName)
+                        .build())
+                .collect(Collectors.toSet());
+    }
+
+    private ComputeService getComputeServiceFromInfastructureName(String infrastructureName) {
+        return computeServiceCache
+                .getComputeService(infrastructureService.getInfrastructurebyName(infrastructureName));
+    }
 
 }
