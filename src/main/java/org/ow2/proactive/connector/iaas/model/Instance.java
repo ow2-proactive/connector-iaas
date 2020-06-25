8dabcbad9c10df5129b5c232be4517cfbd688686
Maxime COMPASTIÉ <maxime.compastie@activeeon.com>
2020-06-25 18:50:22 +0200

    Removing sensitive credential from rest log call

--- src/main/java/org/ow2/proactive/connector/iaas/model/Instance.java
+++ src/main/java/org/ow2/proactive/connector/iaas/model/Instance.java
@@ -38,7 +38,6 @@ import lombok.experimental.Wither;
                                "initScript" })
 @Getter
 @AllArgsConstructor
-@ToString
 @NoArgsConstructor
 @Wither
 @Builder
@@ -65,5 +64,22 @@ public class Instance {
     private InstanceScript initScript;
 
     private String customScriptUrl;
+
+    @Override
+    public String toString() {
+        return String.format("{id=%s; tag=%s; image=%s; number=%s; status=%s; hardware=%s; network=%s; credentialUsername=%s; credentialPublicKeyName=%s; options=%s; initScript=%s; customScriptUrl=%s}",
+                             id,
+                             tag,
+                             image,
+                             number,
+                             status,
+                             hardware,
+                             network,
+                             credentials.getUsername(),
+                             credentials.getPublicKeyName(),
+                             options,
+                             initScript,
+                             customScriptUrl);
+    }
 
 }
