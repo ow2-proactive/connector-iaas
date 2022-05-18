c19517943565e5be33a6d8e97c8a96634c2861fb
alijawadfahs <alijawadfahs@gmail.com>
2022-05-18 17:57:50 +0200

    Fix OpenStack Images issue with unrecognized OS family  (#101)
    * add the line that allows you to change the port
    
    * Fix the open stack os family issue
    
    * addressing Maroun's comment
    
    * addressing Khalil's comment
    
    * address Isabelle's comments

--- src/main/java/org/ow2/proactive/connector/iaas/cloud/provider/jclouds/JCloudsProvider.java
+++ src/main/java/org/ow2/proactive/connector/iaas/cloud/provider/jclouds/JCloudsProvider.java
@@ -49,6 +49,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.ow2.proactive.connector.iaas.cloud.TagManager;
 import org.ow2.proactive.connector.iaas.cloud.provider.CloudProvider;
+import org.ow2.proactive.connector.iaas.cloud.provider.jclouds.openstack.OpenstackUtil;
 import org.ow2.proactive.connector.iaas.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -195,15 +196,12 @@ public abstract class JCloudsProvider implements CloudProvider {
                                                                      .arch(it.getOperatingSystem().getArch())
                                                                      .description(it.getOperatingSystem()
                                                                                     .getDescription())
-                                                                     .family(Optional.ofNullable(it.getOperatingSystem()
-                                                                                                   .getFamily())
-                                                                                     .orElse(org.jclouds.compute.domain.OsFamily.UNRECOGNIZED)
-                                                                                     .value())
+                                                                     .family(getOperatingSystemFamily(it,
+                                                                                                      infrastructure.getType()))
                                                                      .is64Bit(it.getOperatingSystem().is64Bit())
                                                                      .build())
                                      .build())
                      .collect(Collectors.toSet());
-
     }
 
     @Override
@@ -444,4 +442,22 @@ public abstract class JCloudsProvider implements CloudProvider {
         }).collect(Collectors.toSet());
         return result;
     }
+
+    /**
+     * @return the Operating system family for images. When the infrastructure type is openstack-nova,
+     * getOpenStackOSFamily method will be called, otherwise retrieve it from the metadata collected by Jclouds.
+     * This allows us to utilise the distro_family parameter for OpenStack images.
+     */
+    private String getOperatingSystemFamily(org.jclouds.compute.domain.Image image, String infrastructureType) {
+        if (!infrastructureType.equals("openstack-nova")) {
+            return Optional.ofNullable(image.getOperatingSystem().getFamily())
+                           .orElse(org.jclouds.compute.domain.OsFamily.UNRECOGNIZED)
+                           .value();
+        } else {
+            return Optional.ofNullable(OpenstackUtil.getOpenStackOSFamily(image))
+                           .orElse(org.jclouds.compute.domain.OsFamily.UNRECOGNIZED)
+                           .value();
+        }
+    }
+
 }
